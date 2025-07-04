a34c53ff35f80925e7fbe1ebc7a88e61b2a1e17a
Mert <mertcansarginn@gmail.com>
2025-07-04 18:44:22 +0300

    update

--- Selenium/HaberTakip/main.py
+++ Selenium/HaberTakip/main.py
@@ -11,14 +11,22 @@ cssValue='css-1jxf684'
 services=Service(driverpath)
 driver=webdriver.Chrome(service=services)
 driver.maximize_window()
-driver.implicitly_wait(3),
+driver.implicitly_wait(5),
 driver.get(link)
 
 
-element = driver.find_element(By.XPATH, '//div[@data-testid="tweetText"]')
-text = element.text
-print(text)
+elements = driver.find_elements(By.XPATH, '//div[@data-testid="tweetText"]')
+time_elements = driver.find_elements(By.TAG_NAME, "time")
 
+print('Element: ',len(elements))
+print('Time: ',len(time_elements))
 
+for element in elements:
+    for time_element in time_elements:
+     print(element.text)
+     print(time_element.text)
+     print("--------------------------------------------------------")
+     break
 
+ 
 time.sleep(10)
